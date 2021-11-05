241710f57bb18d69884912c1dab1e51e65a4d175
Joao Pedro <joaopedrobarsaglini@gmail.com>
2021-11-05 06:17:27 -0300

    progress spinner

--- crud-angular-spring/src/app/courses/courses/courses.component.ts
+++ crud-angular-spring/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,8 @@
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-courses',
@@ -13,12 +14,17 @@ import { Observable } from 'rxjs';
 
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
 
   constructor(private CoursesService : CoursesService) {
-    this.courses = this.CoursesService.list();
+    this.courses$ = this.CoursesService.list().pipe(
+      catchError(error => {
+        console.log(error)
+        return of([])
+      })
+    );
   }
 
   ngOnInit(): void {
