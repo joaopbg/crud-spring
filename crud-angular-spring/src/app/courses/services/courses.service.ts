c345efd26285387b96c228b409a86c1e2b674da0
João Pedro <joaopedrobarsaglini@gmail.com>
2021-11-13 14:19:18 -0300

    pipes implemented

--- crud-angular-spring/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/src/app/courses/services/courses.service.ts
@@ -9,14 +9,14 @@ import { delay, first, tap } from 'rxjs/operators';
 })
 export class CoursesService {
 
-  private readonly API = '/Aassets/courses.json';
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient : HttpClient) { }
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(8000),
+      delay(1000),
       tap(courses => console.log(courses))
     )
   }
